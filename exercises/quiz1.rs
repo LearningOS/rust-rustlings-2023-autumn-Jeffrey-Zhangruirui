5677efcd19fa5647ec67dc6b1ba040363390879c
Jeffrey-Zhangruirui <114602685+Jeffrey-Zhangruirui@users.noreply.github.com>
2023-10-10 15:23:35 +0000

    init

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,13 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(cnt:i32)->i32 {
+    if(cnt<=40){return cnt*2;}
+    cnt
+}
 
 // Don't modify this function!
 #[test]
