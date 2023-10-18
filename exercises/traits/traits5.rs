ee5d59c58982c19245c63aa440c4b592743aba53
Jeffrey-Zhangruirui <114602685+Jeffrey-Zhangruirui@users.noreply.github.com>
2023-10-18 14:16:19 +0000

    init

--- exercises/traits/traits5.rs
+++ exercises/traits/traits5.rs
@@ -7,7 +7,8 @@
 // Execute `rustlings hint traits5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
+
 
 pub trait SomeTrait {
     fn some_function(&self) -> bool {
@@ -29,12 +30,16 @@ impl OtherTrait for SomeStruct {}
 impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
-// YOU MAY ONLY CHANGE THE NEXT LINE
-fn some_func(item: ??) -> bool {
+// Update the function signature to use trait bounds
+fn some_func<T>(item: &T) -> bool
+where
+    T: SomeTrait + OtherTrait,
+{
     item.some_function() && item.other_function()
 }
 
 fn main() {
-    some_func(SomeStruct {});
-    some_func(OtherStruct {});
+    some_func(&SomeStruct {});
+    some_func(&OtherStruct {});
 }
+
