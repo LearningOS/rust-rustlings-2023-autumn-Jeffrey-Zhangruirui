252593dd9de2fcdcf38ee8ad6e84d5d8965a846d
Jeffrey-Zhangruirui <114602685+Jeffrey-Zhangruirui@users.noreply.github.com>
2023-10-20 14:40:27 +0000

    init

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -3,19 +3,16 @@
 // Execute `rustlings hint macros4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[rustfmt::skip]
 macro_rules! my_macro {
     () => {
-        println!("Check out my macro!");
+        println!("This is the first macro!");
     }
-    ($val:expr) => {
-        println!("Look at this other macro: {}", $val);
-    }
+    
 }
 
 fn main() {
-    my_macro!();
-    my_macro!(7777);
+    my_macro!();      // 调用第一个宏规则，没有参数
+ 
 }
+
