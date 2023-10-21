31562ad90fb510baace1c4976a3051e5cf3cecd7
Jeffrey-Zhangruirui <114602685+Jeffrey-Zhangruirui@users.noreply.github.com>
2023-10-21 09:42:19 +0000

    init

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,12 +40,40 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
+
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            // If the string is empty, return the default Person
+            return Person::default();
+        }
+
+        let parts: Vec<&str> = s.split(',').collect();
+        if parts.len() != 2 {
+            // If there aren't exactly two parts (name and age), return the default Person
+            return Person::default();
+        }
+
+        let name = parts[0].to_string();
+        if name.is_empty() {
+            // If the name is empty, return the default Person
+            return Person::default();
+        }
+
+        if let Ok(age) = parts[1].parse::<usize>() {
+            // If parsing the age succeeds, return the Person with the parsed values
+            return Person {
+                name,
+                age,
+            };
+        } else {
+            // If parsing the age fails, return the default Person
+            return Person::default();
+        }
     }
 }
+
 
 fn main() {
     // Use the `from` function
